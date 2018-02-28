f849a2ad8016209fe485f016d1badd979f1d80d8
arcadio1894 <joryes1894@gmail.com>
2018-02-27 22:36:39 -0500

    Move datos to controller

--- controladores/UsuarioController.php
+++ controladores/UsuarioController.php
@@ -1,20 +1,73 @@
 <?php 
-	include "../datos/usuarioDatos.php";
+	include "../modelos/usuario.php";
+	include "conexion.php";
 
-	class usuarioController{
+	class usuarioController extends conexion {
+		public function __construct(){
+	        $usuarios = new usuario();
+	    }
+
 		function insertarUsuario($email, $username,$password){
-		    $obj = new usuarioDatos();
-		    return $obj->insertarUsuario($email,$username, $password);
+		    $conn = $this->conectar();
+		
+			$usuario = new usuario();
+			$usuario->email = $email;
+			$usuario->username = $username;
+			$usuario->password = base64_encode($password);
+			$usuario->rol = 1;
+
+			$sql = $conn->prepare("INSERT INTO usuario(email, username, password, rol)
+			    VALUES(:email, :username, :password, :rol)");
+			$result = $sql->execute(array(
+			    "email" => $usuario->email,
+			    "username" => $usuario->username,
+			    "password" => $usuario->password,
+			    "rol" => $usuario->rol
+			));
+
+			if($result){
+				return true;
+			}else{
+				return false;
+			}
 		}
 
 		function validarUsuario($username,$password){
-			$obj = new usuarioDatos();
-		    return $obj->validarUsuario($username,$password);
+			$conn = $this->conectar();
+			$usuario = new usuario();
+			$usuario->username = $username;
+			$usuario->password = base64_encode($password);
+
+			$sql = $conn->prepare('SELECT * FROM usuario WHERE username = :username AND password = :password');
+			$sql->execute(array(
+					'username' => $usuario->username,
+					'password' => $usuario->password
+				));
+			$resultado = $sql->fetchAll();
+	        
+	        if( $resultado>0 ){
+	            return $resultado;
+	        }
 		}
 		/*
 		function getId($usuario,$pass){
-			$obj = new usuarioDatos();
-			return $obj->getId($usuario,$pass);
+			$con = $this->conectar();
+			$usuarios = new usuarios();
+			$usuarios->usuario=$usuario;
+			$usuarios->contrasena = base64_encode($pass);
+	        
+			mysqli_select_db($con,"formLogin");
+	        
+			$sql = "SELECT * FROM usuarios WHERE usuario='".$usuarios->usuario."' and contrasena='".$usuarios->contrasena."'";
+	        $consulta = mysqli_query($con,$sql);
+	        $fila = mysqli_fetch_array($consulta);
+	        if($fila>0){
+	            if($fila["usuario"] == $usuarios->usuario && $fila["contrasena"]==$usuarios->contrasena){
+	                return json_encode($fila);
+	            }
+	        }else{
+	            return "";
+	        }
 		}*/
 	}
 ?>
